11bd7a313310af5b94e29640e3b7bb8a938b6df2
eporcello <eve@moonhighway.com>
2015-08-25 13:33:30 -0700

    Adds SASS/SCSS sample to repo

---
+++ grunt-scss/Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'style/style.css' : 'style.scss'
+                }
+            }
+        },
+        cssmin: {
+          target: {
+              files: [{
+                  expand: true,
+                  cwd: 'style/',
+                  src: ['*.css', '!.min.css'],
+                  dest: 'release/',
+                  ext: '.min.css'
+              }]
+          }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.registerTask('default', ['sass', 'watch']);
+}
