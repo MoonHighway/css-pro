7e85af9c26df046fdc7ca0cadee1e7c6ba81a412
Alex Banks <alex@moonhighway.com>
2015-08-25 19:58:01 -0700

    Updates to map file

--- grunt-scss/Gruntfile.js
+++ grunt-scss/Gruntfile.js
@@ -27,6 +27,6 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-cssmin')
-    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['sass', 'cssmin', 'watch' ]);
 }
